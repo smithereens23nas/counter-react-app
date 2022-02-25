a7eb591120e5e09068c215d41949deff6ff3bf9c
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-25 18:15:28 -0500

    started my counter react app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/Counter'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Counter />,
   document.getElementById('root')
 );
 
