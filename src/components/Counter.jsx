c387c10e3c13bbf9a662b0edb0e17bd1c6da2e11
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-27 18:48:41 -0500

    added handleIncrement function and handleClick

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,21 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     };
 
+    handleIncrement = () => {
+
+        console.log('Clicked', this)
+    }
+
     render(){
+
         return (
-            <div>        
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+            <div> 
+                <span className={ this.getBadgeClasses() }>{this.formatCount()}</span>
+                <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+
             </div>
 
         ) 
     }
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
     formatCount(){
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
+    
 }
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from './components/Counter'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
     <Counter />,
